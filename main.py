7d5f236d5500a92b40c5811a9bc5ff74427642ee
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-28 22:47:46 +0300

    take all

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-bot = Bot(token='')
+from config import TOKEN
+
+import random
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-async def main():
-    await dp.start_polling()
+@dp.message(F.photo)
+async def react_photo(message: Message):
+    list = ['Ого, какая фотка!', 'Непонятно, что это такое', 'Не отправляй мне такое больше']
+    rand_answ = random.choice(list)
+    await message.answer(rand_answ)
+@dp.message(F.text == "Что такое ИИ?")
+async def aitext(message: Message):
+    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+
+@dp.message(Command('help'))
+async def help(message: Message):
+    await message.answer("Этот бот умеет выполнять команды: \n /start \n /help")
 
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer("Приветики, я бот!")
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
